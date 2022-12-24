0b67067b408094c9c1ca383cfe9a10b9f3ef1f0b
etitievsky <etitievsky@mail.ru>
2022-12-24 09:52:57 +0300

    Task 03

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void FillArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+}
+
+
+int [] array = new int [8];
+FillArray(array);
+Console.Write($"[{String.Join(",",array)}]");
